fa73888aad7d10bbc131009d8cd1e34df6e43c9b
Carl <Carl@PWC_PC02TDGX.fritz.box>
2016-08-03 00:02:44 +0200

    Hashtables (Regale und Schuhe) initiiert, ID generatoren angelegt.

--- exp/SchuLa.java
+++ exp/SchuLa.java
@@ -4,28 +4,116 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-public class Lager implements Serializable{
+public class SchuLa implements Serializable{
+	/*
+	 * @author: Carl L. Fritze
+	 * @ansatz: SchuLa bildet den Single-point-of-Access, also den ausgewiesenen Anknuepfpunkt für 
+	 * alle Bestandteile, die nicht zwingender Bestandteil der Software selbst sind.
+	 * Nutzer sollen in der Lage sein, mittels der oeffentlichen Methoden dieser Klasse die gewuenschten
+	 * Aktionen durchzuführen. Dies soll Folgendes bezwecken:
+	 * 	- einfachere Anbindung von Komponenten (etwa GUI, andere Systemschnittstellen)
+	 *  - Konsistenz: da Einzelheiten der Software nicht gezielt ansteuerbar sind, sollen Manipulationen der Inhalte vermieden werden.
+	 *  - Transparenz: Entwicklern soll einfacher gemacht werden, den Quelltext nachzuvollziehen, in dem einzelne Szenarien gezielt verfolgt werden koennen.
+	 *  
+	 * @aufbau: jede Klasse von SchuLa ist jeweils in zwei Bereiche unterteilt:
+	 * 	1. oeffentliche Methoden: zu erkennen an dem Kennzeichen "public" stellen diese eine Komponente
+	 * 	   des Programmes dar, die von außen ueber den Aufruf der Klasse direkt angesteuert werden koennen.
+	 *  2. geschützter Bereich: in diesem Bereich sind anschließend Konstanten, Variablen, Funktionen 
+	 *     und Methoden definiert, die nur für die Software ansteuerbar sein sollen.
+	 *     In diesem Bereich sollte das Access Level "public" gemieden werden.
+	 *     Grundsaetzlich sollten Datenelemente nicht direkt auf public gesetzt werden. Ebenso sollte eine
+	 *     Aenderung der gespeicherten Informationen nur ueber die Methoden der Klasse SchuLa oder entsprechend
+	 *     geschuetzte Methoden der Einzelklassen zugelassen werden. Ein solcher Schutz kann etwa die Forderung
+	 *     einer schwer zu erratenden Zeichenkette als notwendiger Parameter für diese kritischen Routinen sein,
+	 *     die wiederum selbst als privates Datenelement in der Klasse gespeichert wird.
+	 */
 	
-	//Festlegung von lager-weiten Variablen: 
+//oeffentlicher Bereich: ALLE UNFERTIG
 	
-	//Eine Fortlaufende Nummer ueber alle Schuhmodelle wird gefuehrt
+	//erhalte ein Feld von Schuhen (eine Liste) von Schuhen, die im System gespeichert sind.
+	//Mitgegeben werden soll ein Wert der Klasse Schuh, in dem alle gesuchten Merkmale ausgeprägt sind, die Anderen leer.
+	//Die Methode gibt Schuhe sortiert nach Anzahl der übereinstimmenden Merkmale maximal in Länge des Limits zurück.
+	//Ist Limit auf "0" gesetzt, werden alle Schuhe mitgegeben, die in mindestens einem Merkmal übereinstimmen.
+	public Schuh[] getSchuhlist(Schuh Vergleichswert, int Limit){
+		Schuh[] suchergebnis = null;
+		return suchergebnis;
+	}
+	//Wird kein Limit mitgegeben, werden alle Ergebnisse mit mindestens einer Übereinstimmung zurückgegeben.
+	public Schuh[] getSchuhlist(Schuh Vergleichswert){
+		Schuh[] suchergebnis = getSchuhlist(Vergleichswert, 0);
+		return suchergebnis;
+	}
+	
+	//Ein Schuh soll mittels einer SchuhID oder KartonID auffindbar sein.
+	//Welche Form Identifier sollte dabei von der Funktion selbst evaluiert werden.
+	public Fach[] finde(String Identifier){
+		Fach[] gefundenIn = null;
+		return gefundenIn;
+	}
+	
+//geschuetzte Objekte:
+	/* 
+	 * @Itr: Um eine eindeutige, instanzweite Identifizierung zu ermöglichen, wird fuer alle wiederkehrende Elemente 
+	 * je eine fortlaufende Nummer gefuehrt, die allerdings nicht direkt vergeben wird. Stattdessen ist sie ueber 
+	 * eine jeweilige ID generierende Methode geschützt automatisiert abrufbar (siehe untern)
+	 */
+	
+	//fortlaufende Nummer fuer Karton-IDs (=Anzahl unterscheidbarer Kartons im System seit Installation)
 	private static int KartonItr;
-	//um manipulation zu schützen, ist das abrufen einer neuen ID erlaubt, 
-	//aber erhoehe den Zaehler der erfassten Modelle um 1 erhoeht...
-	protected static String getKartonID(){
+		
+	//fortlaufende Nummer fuer unterscheidbare Schuhe (Anzahl verschiedenartiger Schuhe um mindestens 1 Merkmal)
+	private static int SchuhItr;
+	
+	/*
+	 * @Regal: fortlaufende Nummer fuer Regale erlaubt eine eindeutige Identifizierung (per Namensgebung) fuer Regale
+	 * Faecher werden systematisch vergeben. Es wird unterstellt, dass ein Regal gleichgrosse rechtwinklige Faecher
+	 * umfasst. Diese werden mittels Entfernung vertikaler und horizontaler Entfernung von einer der Kanten nummeriert.
+	 * Dazu wird bei Anlage des Regals jedem Fach ein Zeilen und ein Spaltenwert zur Identifikation zugeordnet.
+	 * Bei unterschiedlich großen Fächern sind mehrere separate "fiktiv getrennte" Regale zu erfassen.
+	 */
+	private static int RegalItr;
+	
+	/*
+	 * @Lager: Das Lager wird mittels einer Hashtable von String(RegID) und dem Regal selbst abgebildet.
+	 * Die Entscheidung zu dieser Umsetzung lässt sich wie folgt begründen:
+	 *  + Eine Hashtable ist beliebig um Elemente erweiterbar und bietet somit einen klaren Vortiel hinsichtlich
+	 *    der Flexibilität und Erweiterbarkeit gegenüber Feldern
+	 *  + Über eine Hashtable kann gezielt nach einem bestimmten Element innerhalb des Objektes gesucht werden.
+	 *  + Eine Hashtable laesst einen Parallelen Zugriff zu. Unter dem Gedanken, dass perspektivisch mehrere Anfragen
+	 *    parallel gestellt würden, ließe dies einen performanten Vorteil zu.
+	 *  + Eine Hashtable sichert von sich aus, dass sie lückenfrei bleibt und sichert somit Konsistenz.
+	 *  Als Werte wurden die Klasse Regal (unmittelbar) gewählt, da so das Objekt direkt referenziert wird und somit
+	 *  die verwalteten Regal nicht als separate Objekte und Klassen außerhalb der Umgebung ansteuerbar sein mussten.
+	 *  Als Key wurde jeweils die Regal-ID gewählt, um so die Transparenz über die Klassen zu halten.
+	 *  "Ein Regal wird durch die ID gefunden. Ein Regal selbst weisst gleiche ID als Konstante aus, um ueberpruefbar 
+	 *  und verfolgbar zu sein.
+	 */
+	private Hashtable<String, Regal> lagerplatz;
+	
+	private Hashtable<String, Schuh> schuhkatalog;
+	
+// geschuetzte Methoden:
+	/*
+	 * @ID-Vergabe: Beim Erfassen neuer Elemente (Schuh, Karton, Regal) ruft der Konstruktor der jeweiligen Klasse folgende
+	 * Methode auf, die in Abhängigkeit des anfragenden Objektes einen entsprechenden eindeutigen Identifier generiert.
+	 * Um die Eindeutigkeit zu sichern, wird der zuvor definierte Iterator verwendet, der bei jeder ID Anfrage den jeweiligen
+	 * Klassenzaehler um eins erhoeht. 
+	 */
+	protected static String generateKartonID(){
 		return Integer.toString(++KartonItr, 16);
 	}
+	protected static String generateSchuhID(){
+		return Integer.toString(++SchuhItr, 16);
+	}
+	protected static String generateRegalID(){
+		return Integer.toString(++RegalItr, 16);
+	}
 	
-	//Fortlaufende Nummer unterscheidbarer Schuharten:
-	//wie für Kartons ist eine neue ID abrufbar, dabei wird der Zaehler um 1 erhoeht.
-	private static int SchuhIter;
-	protected static String getSchuhID(){
-		return Integer.toString(++SchuhIter, 16);
-	}
-	private static int RegalIter;
-	protected static String getRegalID(){
-		return Integer.toString(++RegalIter, 16);
-	}
+	
+// ##################################
+	
+	
+// ALTER ENTWURF: in Schema zu uebertragen...	
 	
 	/*Abbildung des "Lagers" --> Sammlung von Regalen, durch Hashtable hier flexibel gehalten,
 	 * um eine Erweiterung und dabei konsistente Haltung zu ermöglichen.
@@ -35,7 +123,6 @@ public class Lager implements Serializable{
 	 * nur eben woanders als im Lager entweder also beim Kunden zur Anprobe, in Reinigung oder im Verkauf
 	 * bei Verkauf, würde der Karton finalized werden.
 	 */
-	private Hashtable<String, Regal> lagerplatz;
 	protected void addRegal(){
 		Regal r = new Regal();
 		this.lagerplatz.put(r.name(), r);
