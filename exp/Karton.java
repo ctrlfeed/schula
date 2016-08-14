ddc0f3c57f21756cca952cee812e05a9b91fb1df
Carl <Carl@PWC_PC02TDGX.fritz.box>
2016-08-14 15:18:55 +0200

    Regal lauffähig. Mit "Fach"-informationen...

--- exp/Karton.java
+++ exp/Karton.java
@@ -1,39 +1,45 @@
 package exp;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
-public class Karton extends Schuh{
+public class Karton extends Schuh implements Serializable{
 	private Fach adresse = null;
 	private String kartonID;
-	private Timestamp zugegangen;
+	private Timestamp zugang = null;
+	private Timestamp abgang;
 	
-	/*
-	 * Konstruktoren
-	 */
+// Konstruktoren
+
 	public Karton(){ 
-		this.zugegangen = new Timestamp(System.currentTimeMillis());
-		this.kartonID = generateKartonID();
-		lassliegen(this);
+		this.zugang = new Timestamp(System.currentTimeMillis());
+		this.kartonID = SchuLa.generateKartonID();
+		this.ablegen(SchuLa.umlauf);
 	}
 	public Karton(Fach adr){
 		this();
 		this.adresse = adr;
 	}
-	/*
-	 * Methoden
-	 */
-	public void ablegen(Fach whereto) throws fachvollException {
+
+// oeffentliche Methoden
+	 public void setFach(Fach whereto) throws fachvollException {
 		this.adresse.nehme(this);
 		whereto.belegen(this);
 		this.adresse = whereto;
 	}
-	public Fach whereat(){
+	public Fach getFach(){
 		return this.adresse;
 	}
-	public boolean isUmlauf(){
-		return this.adresse.equals(umlauf);
+	public boolean isVorhanden(){
+		Timestamp now = new Timestamp(System.currentTimeMillis());
+		return ((this.zugang != null) && (this.abgang.after(now)));
 	}
 	public String getID(){
 		return this.kartonID;
+	}
+	public void ablegen(Fach neuerOrt){
+		this.adresse.nehme(this);
+		neuerOrt.belegen(this);
+		this.adresse = neuerOrt;
 	}
 }
--- exp/Regal.java
+++ exp/Regal.java
@@ -1,12 +1,14 @@
 package exp;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
-public class Regal extends SchuLa{
+public class Regal implements Serializable{
 	private Hashtable<String, Fach> faecher;
 	private String bezeichner;
+	private int kapazitaet;
 	
 	/* Konstruktoren:
 	 * ohne Parameter -> Annahme der Dimensionen 1x1 und Kapazitaet 1;
@@ -25,13 +27,14 @@ public class Regal extends SchuLa{
 	}
 	protected Regal(int breite, int hoehe, int kap){
 		if (breite > 0 && hoehe > 0){
-			this.bezeichner = generateRegalID();
+			this.bezeichner = SchuLa.generateRegalID();
 			for (int i = 1; i <= breite; i++) { //Spalten im Regal
 				for (int j = 0; j < hoehe; j++) { //Zeilen im Regal
 					Fach f = new Fach(this.bezeichner + "." + Integer.toString(i) + "-" + Integer.toString(j), kap);
-					this.faecher.put(f.name(), f);
+					this.faecher.put(f.getID(), f);
 				}
 			}
+			this.kapazitaet = breite * hoehe * kap;
 		} else {
 			//throw unzulaessige-dimensionen-exception
 			System.out.println("ein derartiges Regal lässt sich nicht anlegen.");
@@ -41,14 +44,14 @@ public class Regal extends SchuLa{
 	/*
 	 * Methoden:
 	 */
-	public String name(){
+	public String getID(){
 		return bezeichner;
 	}
 	
 	//ueberpruefe alle Faecher auf freie Plaetze
 	public Fach[] getFreiraum(){
 		List<Fach> freiFaecher = new ArrayList<Fach>();
-		//freiFaecher ist das Result-Set als Variable Länge definiert...
+		//freiFaecher ist das Result-Set in Variabler Länge definiert...
 		//Überprüfung jedes Faches, wie viele/ob noch Plätze für Kartons frei sind
 		for (Fach potentialfrei : this.faecher.values()) {
 			if (potentialfrei.freiraum()>0){
@@ -67,5 +70,11 @@ public class Regal extends SchuLa{
 		}
 		return freiplatz;
 	}
-
+	public int auslastung(){
+		int a = this.getFreiplatz(this.getFreiraum());
+		return a;
+	}
+	public boolean isEmpty(){
+		return (this.kapazitaet == this.auslastung());
+	}
 }
